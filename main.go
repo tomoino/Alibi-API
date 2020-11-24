6b727716a5ff7382ae0814da7cc1ebdf6dcad4ae
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2020-11-24 22:03:53 +0900

    [add]main.go
    Hello World

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo"
+)
+
+// この関数を追加
+func port() string {
+
+	port := os.Getenv("PORT")
+
+	if len(port) == 0 {
+		port = "8080"
+	}
+
+	return ":" + port
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	// Port番号を関数から取得
+	e.Logger.Fatal(e.Start(port()))
+}
