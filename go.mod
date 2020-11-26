b21a3d802d78c58fec77eee186b0646e1d1a458e
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2020-11-26 23:46:10 +0900

    [update]main: add env
    envファイルを追加

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.3.0
 	github.com/labstack/echo/v4 v4.1.17
 )
--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
@@ -34,7 +36,15 @@ func connect() {
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		err := godotenv.Load(fmt.Sprintf("./%s.env", os.Getenv("GO_ENV")))
+		databaseUrl := ""
+		if err != nil {
+			// .env読めなかった場合の処理
+			databaseUrl = "ENVないよ！"
+		} else {
+			databaseUrl = os.Getenv("DATABASE_URL")
+		}
+		return c.String(http.StatusOK, "Hello, World!"+databaseUrl)
 	})
 
 	// Port番号を関数から取得
